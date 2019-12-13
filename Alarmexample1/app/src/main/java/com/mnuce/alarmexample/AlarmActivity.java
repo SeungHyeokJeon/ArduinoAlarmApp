d8280a7a6800b2631bf7a6348e9170e4f57dd5bd
SeoungHyuckJeon <sdkazx2717@gmail.com>
2019-12-13 19:52:10 +0900

    Alarmexample1에 블루투스 권한 설정

---
+++ Alarmexample1/app/src/main/java/com/mnuce/alarmexample/AlarmActivity.java
@@ -0,0 +1,59 @@
+package com.mnuce.alarmexample;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        // 알람음 재생
+        this.mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        this.mediaPlayer.start();
+
+        findViewById(R.id.btnClose).setOnClickListener(mClickListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // MediaPlayer release
+        if (this.mediaPlayer != null) {
+            this.mediaPlayer.release();
+            this.mediaPlayer = null;
+        }
+    }
+
+    /* 알람 종료 */
+    private void close() {
+        if (this.mediaPlayer.isPlaying()) {
+            this.mediaPlayer.stop();
+            this.mediaPlayer.release();
+            this.mediaPlayer = null;
+        }
+
+        finish();
+    }
+
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnClose:
+                    // 알람 종료
+                    close();
+
+                    break;
+            }
+        }
+    };
+}
